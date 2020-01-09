bfdac4a8a0bb108c1d9c3b99b51c840f0cde9a73
Pedro Guilherme <47400938+pedroschmid@users.noreply.github.com>
2020-01-09 03:24:14 -0300

    Update server.js

--- StarWars-CRUD/server.js
+++ StarWars-CRUD/server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const MongoClient = require('mongodb').MongoClient
-const url = 'mongodb+srv://root:root@cluster0-zp9uk.mongodb.net/test?retryWrites=true&w=majority'
+const url = 'mongodb+srv://root:<password>@cluster0-zp9uk.mongodb.net/test?retryWrites=true&w=majority'
 
 // Initializing express
 const app = express()
